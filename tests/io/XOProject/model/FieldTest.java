352151a809c818f99d28b729de76873747402d99
exzemko <exzeme@mail.ru>
2017-09-30 16:59:10 +0300

    Field checks addeddd

--- tests/io/XOProject/model/FieldTest.java
+++ tests/io/XOProject/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.XOProject.model;
 
+import io.XOProject.model.Exceptions.AlreadyOccupiedException;
 import io.XOProject.model.Exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -92,6 +93,22 @@ public class FieldTest {
             field.getFigure(inputPoint);
             fail();
         } catch (final InvalidPointException e) {}
+
+    }
+
+    @Test
+    public void testGetFigureWhenWhenAlreadyOccupied() throws Exception {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
 
     }
 }

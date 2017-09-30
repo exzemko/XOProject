05ad9e15d12b19bb095430ddd9e95de520cbf43d
exzemko <exzeme@mail.ru>
2017-09-30 16:29:45 +0300

    Field - test for the Point added

--- src/io/XOProject/model/Field.java
+++ src/io/XOProject/model/Field.java
@@ -1,10 +1,19 @@
 package io.XOProject.model;
 
+import io.XOProject.model.Exceptions.AlreadyOccupiedException;
+import io.XOProject.model.Exceptions.AlreadyOccupiedException;
+import io.XOProject.model.Exceptions.InvalidPointException;
+
 import java.awt.*;
+import java.rmi.AlreadyBoundException;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private  final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -14,13 +23,34 @@ public class Field {
 
     }
 
-    public Figure getFigure (final Point point){
+    public Figure getFigure (final Point point) throws InvalidPointException{
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
 
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure){
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
+                                                                         AlreadyOccupiedException{
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null){
+            throw new AlreadyOccupiedException();
+        }
 
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint (final Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+
+    }
+
+    private  boolean checkCoordinate (final  int coordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+
+
     }
 }

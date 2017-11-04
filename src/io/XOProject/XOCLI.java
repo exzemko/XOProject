2617927c8d1fb03d92abe36f73851f2d188deea9
exzemko <exzeme@mail.ru>
2017-11-04 17:40:13 +0300

    XOCLI - created

---
+++ src/io/XOProject/XOCLI.java
@@ -0,0 +1,26 @@
+package io.XOProject;
+
+import io.XOProject.model.Field;
+import io.XOProject.model.Figure;
+import io.XOProject.model.Game;
+import io.XOProject.model.Player;
+import io.XOProject.view.ConsoleView;
+
+public class XOCLI {
+    public static void main(String[] args) {
+        final String name1 = "Gleb";
+        final String name2 = "Slava";
+
+        final Player[] players = new Player[2];
+        players[0] = new Player(name1, Figure.X);
+        players[1] = new Player(name2, Figure.O);
+
+        final Game gameXO = new Game(players, new Field(3), "XO");
+
+        final ConsoleView consoleView = new ConsoleView();
+        consoleView.show(gameXO);
+        while (consoleView.move(gameXO)) {
+            consoleView.show(gameXO);
+        }
+    }
+}
